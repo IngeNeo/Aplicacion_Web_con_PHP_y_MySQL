5f4b8c841b57f491d218c11f4e796dafe4b1c6be
Roger Valencia <roger.valencia.garcia@hotmail.com>
2023-07-22 16:03:10 -0500

    Pruebas de HTML

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Aplicacion_Web_con_PHP_y_MySQL
+# Aplicación_Web_con_PHP_y_MySQL
 Aplicación Web con PHP y MySQL
 
 Curso basado en el Libro Desarrollo de aplicaciones web con PHP y MySQL
---
+++ src/Sentencias MySQL para el objeto de base de datos.sql
@@ -0,0 +1,12 @@
+/**
+* Examples
+** Sintaxis de la sentencia SQL
+*! Tener precaución al usarlo
+*? Realizar la prueba de salida de datos
+*TODO: Ejecutar las sentencias
+*@param create
+*/
+/**
+** Creación base de datos
+CREATE DATABASE [IF NOT EXISTS] NOMBRE_BASE_DATOS
+*/
