fbdb9843b33f72b2e60e4e19352320de6e5e0300
Roger Valencia <roger.valencia.garcia@hotmail.com>
2023-08-09 17:18:37 -0500

    Pruebas de conexion de PHP y MySQL

---
+++ conexion1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style/style.css">
+	<title>Conexión con PHP a MySQL</title>
+</head>
+<body></body>
+	<?php
+		$servername = "localhost";
+		$database = "ventas2023";
+		$username = "root";
+		$password = "";
+		// Crear conexión
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		// Validando la conexión
+		if (!$conn) {
+		    die("La conexión fallo: " . mysqli_connect_error());
+		}
+		echo "La conexión fue exitosa!";
+		mysqli_close($conn);
+	?>
+</body>
+</html>
---
+++ php/Sentencias.md
@@ -0,0 +1,10 @@
+--* Conectar MySQL a PHP
+--? Función de conexión MySQL
+
+mysqli_connect($servername, $username, $password, $database);
+
+-- $servername: Es el nombre del servidor; en entornos de prueba sera probable que este sea "localhost"
+-- $username: Es el nombre del usuario de la base de datos
+-- $password: Es la clave del usuario de la base de datos que vamos a usar
+-- $database: Nombre de la base de datos a la que nos vamos a conectar
+
